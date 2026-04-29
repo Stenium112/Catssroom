ea3283b594a0b95196186eec5608e75784976b93
Stenium <erwan24270@hotmail.com>
2026-04-29 16:19:31 +0200

    lil update

--- widget/barre.gd
+++ widget/barre.gd
@@ -1,6 +1,6 @@
 extends Window
 
-var mouse_hover : bool = false
+var mouse_offset : Vector2i
 
 @onready var panel: Panel = $Panel
 
@@ -11,12 +11,7 @@ func _on_hide_pressed() -> void:
 	Global.hidden = true
 
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("left click") and mouse_hover:
-		pass
-
-func _on_mouse_entered() -> void:
-	mouse_hover = true
-
-
-func _on_mouse_exited() -> void:
-	mouse_hover = false
+	if Input.is_action_just_pressed("left click"):
+		mouse_offset = panel.get_local_mouse_position()
+	if Input.is_action_pressed("left click"):
+		position = (position + Vector2i(panel.get_global_mouse_position()) - mouse_offset)
