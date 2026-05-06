380b74ce493c6204c9fef0ac56b0896e037394ab
Stenium <erwan24270@hotmail.com>
2026-05-06 16:25:20 +0200

    Replace the method of window deplacement to start_drag().

--- chrono.gd
+++ chrono.gd
@@ -20,10 +20,6 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	arrange_string(time)
-	#if Input.is_action_just_pressed("left click"):
-		#mouse_offset = panel.get_local_mouse_position()
-	#if Input.is_action_pressed("left click"):
-		#chrono.position = (chrono.position + Vector2i(panel.get_global_mouse_position()) - mouse_offset)
 	
 	if Input.is_action_pressed("left click"):
 		chrono.start_drag()
--- widget/barre.gd
+++ widget/barre.gd
@@ -11,7 +11,5 @@ func _on_hide_pressed() -> void:
 	Global.hidden = true
 
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("left click"):
-		mouse_offset = panel.get_local_mouse_position()
 	if Input.is_action_pressed("left click"):
-		position = (position + Vector2i(panel.get_global_mouse_position()) - mouse_offset)
+		start_drag()
