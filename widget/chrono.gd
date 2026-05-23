6db67a96aded0c967b252b7ab3b91c71e144882d
Stenium <erwan24270@hotmail.com>
2026-05-23 16:15:17 +0200

    Re-managed the files

--- widget/chrono.gd
+++ widget/chrono.gd
@@ -20,7 +20,6 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	print(mouse_in)
 	arrange_string(time)
 	
 	if scale < Vector2(0.25, 0.25): scale = Vector2(0.25, 0.25)
