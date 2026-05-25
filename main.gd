75804e5b2ce1b0411f73dbebd4d773d7056d3d08
Stenium <erwan24270@hotmail.com>
2026-05-25 17:16:31 +0200

    Integrated almost all of passtrough logic
    I'm corriging a bug

--- main.gd
+++ main.gd
@@ -1,3 +1,24 @@
 extends Node
 
 @onready var barre: Panel = $Barre
+
+func _ready() -> void:
+	set_mouse_passthrough(get_children())
+
+func set_mouse_passthrough(nodes: Array[Node]):
+	var region: PackedVector2Array = []
+	for node in nodes:
+		if node is Panel:
+			var real_size: Vector2 = node.size * node.scale
+			var global_pos: Vector2 = node.global_position
+			
+			
+			region.insert(0, global_pos)
+			region.insert(0, Vector2(global_pos.x + real_size.x, global_pos.y))
+			region.insert(0, global_pos + real_size)
+			region.insert(0, Vector2(global_pos.x, global_pos.y + real_size.y))
+			
+	
+	$Polygon2D.polygon = region
+	DisplayServer.window_set_mouse_passthrough(region)
+	print(region)
