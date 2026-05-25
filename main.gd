2568c924842e30118124c84da8606c24ec859d70
Stenium <erwan24270@hotmail.com>
2026-05-25 17:52:29 +0200

    same

--- main.gd
+++ main.gd
@@ -5,13 +5,15 @@ extends Node
 func _ready() -> void:
 	set_mouse_passthrough(get_children())
 
+func _process(delta: float) -> void:
+	set_mouse_passthrough(get_children())
+
 func set_mouse_passthrough(nodes: Array[Node]):
 	var region: PackedVector2Array = []
 	for node in nodes:
 		if node is Panel:
 			var real_size: Vector2 = node.size * node.scale
 			var global_pos: Vector2 = node.global_position
-			
 			
 			region.insert(0, global_pos)
 			region.insert(0, Vector2(global_pos.x + real_size.x, global_pos.y))
